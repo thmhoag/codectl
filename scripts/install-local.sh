41027885708f3e9232e06cf29ce147cf1464f636
Thomas Hoag <thmhoag@gmail.com>
2019-10-07 14:32:31 -0400

    Remove packr references from install script

--- scripts/install-local.sh
+++ scripts/install-local.sh
@@ -5,25 +5,16 @@ REPODIR="$(dirname "$SCRIPTDIR")"
 cd "$REPODIR/cmd/codectl"
 
 # always fail script if a cmd fails
-set -e
-
-# fail script if piped command fails
-set -o pipefail
+set -eo pipefail
 
 # Required commands
 command -v go >/dev/null 2>&1 || { echo "go is required but not installed, aborting..." >&2; exit 1; }
 command -v git >/dev/null 2>&1 || { echo "git is required but not installed, aborting..." >&2; exit 1; }
-command -v packr2 >/dev/null 2>&1 || { echo "packr2 is required but not installed, aborting..." >&2; exit 1; }
-
-echo "Using packr2 $(packr2 version)"
 
 BUILD_TIME="$(date +'%m/%d/%Y.%H:%M')"
 COMMIT_HASH="$(git rev-parse --short HEAD)"
 
 echo "Installing..."
-packr2 install -ldflags="-s -w -X main.semver=v0.0.0-dev -X main.commit=$COMMIT_HASH -X main.built=$BUILD_TIME"
-
-echo "Cleaning intermediary files..."
-packr2 clean
+go install -ldflags="-s -w -X main.semver=v0.0.0-dev -X main.commit=$COMMIT_HASH -X main.built=$BUILD_TIME"
 
 echo "Complete!"
