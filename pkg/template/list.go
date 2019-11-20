f1644a308894d7d312883b21adfd9134b4c511ce
Thomas Hoag <thmhoag@gmail.com>
2019-11-20 14:14:41 -0500

    Fix regression on template names for unix systems

--- pkg/template/list.go
+++ pkg/template/list.go
@@ -40,7 +40,7 @@ func GetAll(box *packr.Box, dir string) (map[string]*Properties, error) {
 
 		if props.Name == "" {
 			props.Name = strings.ReplaceAll(filepath.Dir(fileInfo.Name()), "/", ".")
-			props.Name = strings.ReplaceAll(filepath.Dir(fileInfo.Name()), `\`, ".")
+			props.Name = strings.ReplaceAll(props.Name, `\`, ".")
 		}
 
 		templates[props.Name] = props
